b6bc036bbb21b224379c44b2113424e8ec6b1cc8
Monika Grzanek <grzanekmonika@o2.pl>
2024-12-17 10:37:17 +0100

    Update packages

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable indent */
+const app = {
+    init: function(){
+        const thisApp = this;
+        console.log('Welocme to music service!');
+        console.log(thisApp);
+    },
+};
+
+app.init();
+
